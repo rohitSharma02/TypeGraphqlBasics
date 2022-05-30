ff88bd37487b83651ae1f1bdb2f5a5e6d6b2e28d
Rohit Sharma <rohit.sharma02@sjsu.edu>
2022-05-29 23:27:00 -0700

    First hello world -add resolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
 import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import { FirstResolver } from "./resolvers/helloWorld";
+
+(async() => {
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [FirstResolver]
+    }),
+    context: ({ req,res }) => ({ req, res })
+});
+apolloServer.applyMiddleware({ app, cors:false });
+
+app.listen(4000, () => {
+    console.log("Express server started at : 4000");
+});
+});
---
+++ src/resolvers/helloWorld.ts
@@ -0,0 +1,9 @@
+import { Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class FirstResolver{
+    @Query(() => String)
+    hello() {
+        return "hi";
+    }
+}
