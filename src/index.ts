e21677a7b671f5e33a3ffc417e251bdf97d8f546
Rohit Sharma <rohit.sharma02@sjsu.edu>
2022-06-03 13:11:36 -0700

    Boiler plate

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { buildSchema } from "type-graphql";
-import { FirstResolver } from "./resolvers/helloWorld";
+import { buildSchema, Resolver, Query } from "type-graphql";
+//import { FirstResolver } from "./resolvers/helloWorld";
 
-(async() => {
-    const app = express();
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [FirstResolver]
+@Resolver()
+class HelloResolver {
+
+  @Query(()=> String)
+  async hello() {
+    return "Hi testing...";
+  }
+}
+
+const main = async () => {
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver]
     }),
-    context: ({ req,res }) => ({ req, res })
-});
-apolloServer.applyMiddleware({ app, cors:false });
+    context: ({ req, res }) => ({ req, res }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, cors: true });
 
-app.listen(4000, () => {
-    console.log("Express server started at : 4000");
-});
-});
+  app.listen(3000, () => {
+    console.log("Express server started at : http://localhost:3000/");
+  });
+};
+
+main();
